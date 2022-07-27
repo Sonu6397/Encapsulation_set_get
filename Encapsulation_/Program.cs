612300f88496418b3f5e8ef98502ccd5025c2d7f
admin <admin@CHETUIWK1860>
2022-07-27 14:57:43 +0530

    Add project files.

---
+++ Encapsulation_/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Encapsulation_
+{
+    class Employee
+    {
+        public int EmpNo;
+        public string EmpName;
+
+        public void SetEmpNo(int EmpNo)
+        {
+            this.EmpNo = EmpNo;
+           
+        }
+
+        public int GetEmpNo()
+        {
+            return EmpNo;
+          
+        }
+
+        public void SetEmpName(string EmpName)
+        {
+            this.EmpName = EmpName;
+
+        }
+
+        public string GetEmpName()
+        {
+            return EmpName;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee E = new Employee();
+            E.EmpNo = 12;
+            E.EmpName = "sonu kuamr";
+            Console.WriteLine(E.GetEmpNo());
+            Console.WriteLine(E.GetEmpName());
+            Console.ReadLine();
+        }
+    }
+}
